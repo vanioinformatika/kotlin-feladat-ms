b28a97723d7ec874082014bfba4fd93841c6b85a
Kullai Imre <kullai.imre@microsec.hu>
2024-03-21 08:29:28 +0100

    Házi Feladat

--- app/src/main/kotlin/WeatherApp.kt
+++ app/src/main/kotlin/WeatherApp.kt
@@ -8,5 +8,11 @@ class WeatherApp
 
 fun main() {
     runApplication<WeatherApp>()
+
+    val latitudeCoord = 47.4984
+    val longitudeCoord = 19.0404
+    //val latitudeCoord = 47.58446085308398
+    //val longitudeCoord = 18.877541667963346
+    WeatherCalculate.printDayAverageTemperaturesAt( latitudeCoord,  longitudeCoord)
 }
 
